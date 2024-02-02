20272cfa3782b7175b5a697ff5954bab1f454928
chrisGLgithub <githubgl@binner.co.uk>
2024-02-02 16:01:46 +0000

    shows to routes with static text

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+def index():
+    # return app.send_static_file("index.html")
+    # above in the Microsoft vscode-remote-try-python [dev container: python3]
+    return "Welcome to the index"
+
+@app.route('/about')
+def about():
+    return " About us"
+    
+if __name__ == '__main__':
+    app.run()
