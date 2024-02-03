640e425788a487bc1637d5c26c387b13dd83e9d2
chrisGLgithub <githubgl@binner.co.uk>
2024-02-03 02:12:10 +0000

    templates inc all_courses

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
-#to get this to run in the container use run Python Debugger: using launch.json
-from flask import Flask
-app = Flask(__name__)
+# -*- coding: utf-8 -*-
+#!pip install flask-ngrok
 
-@app.route("/")
+from flask import Flask, render_template
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__, template_folder='/templates')
+
+run_with_ngrok(app)
+
+@app.route('/')
 def index():
-    # return app.send_static_file("index.html")
-    # above in the Microsoft vscode-remote-try-python [dev container: python3]
-    # return "Welcome to the index"
-    return "<h1>Welcome</h1>  <p>Under construction</p>"
+  return "<h1>Welcome</h1> <p>Under construction</p>"
 
 @app.route('/about')
 def about():
-    return "About us"
+  return "About Us"
 
-@app.route('/profile/mammoth')    
+@app.route('/profile/mammoth')
 def profile():
-    return "<h2>Welcome to profile</h2>"
+  return "<h2>Welcome to profile Mammoth</h2>"
 
-@app.route('/product/<name>') #<name> is whatever is passed in <> alligator brackets :-)
+@app.route('/product/<name>')
 def product_page(name):
-    return "<h3>Product " + name + "</h3>"
+  return "<h3>Product " + name + "</h3>"
 
 @app.route('/product/<name>/<price>')
-def product_information(name,price):
-    return "<h4>About " + name + ": </h4> <p>Price: " + price + "</p>"
+def product_information(name, price):
+  return "<h4>About " + name + ":</h4> <p>Price: " + price + "</p>"
 
+@app.route('/first-template')
+def first_template():
+  return render_template('first_template.html')
 
+@app.route('/profile/<username>')
+def show_profile(username):
+  return render_template('profile.html', name=username)
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
