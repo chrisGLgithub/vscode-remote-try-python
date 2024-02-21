b208e37bdcaad550a9cdee2305221096d0525ff6
chrisGLgithub <githubgl@binner.co.uk>
2024-02-21 09:42:33 +0000

    total jump to  explore our dataset in Nueral networks

--- app.py
+++ app.py
@@ -1,40 +1,17 @@
-# -*- coding: utf-8 -*-
-#!pip install flask-ngrok
+import pandas
 
-from flask import Flask, render_template
-from flask_ngrok import run_with_ngrok
+movies_dataframe = pandas.read_csv("movies.csv")
 
-app = Flask(__name__, template_folder='/templates')
+movies_dataframe
 
-run_with_ngrok(app)
+ratings_dataframe = pandas.read_csv("ratings.csv")
 
-@app.route('/')
-def index():
-  return "<h1>Welcome</h1> <p>Under construction</p>"
+ratings_dataframe
+unique_movies = ratings_dataframe["movieId"].unique()
+unique_movies
+len(unique_movies)
 
-@app.route('/about')
-def about():
-  return "About Us"
-
-@app.route('/profile/mammoth')
-def profile():
-  return "<h2>Welcome to profile Mammoth</h2>"
-
-@app.route('/product/<name>')
-def product_page(name):
-  return "<h3>Product " + name + "</h3>"
-
-@app.route('/product/<name>/<price>')
-def product_information(name, price):
-  return "<h4>About " + name + ":</h4> <p>Price: " + price + "</p>"
-
-@app.route('/first-template')
-def first_template():
-  return render_template('first_template.html')
-
-@app.route('/profile/<username>')
-def show_profile(username):
-  return render_template('profile.html', name=username)
-
-if __name__ == '__main__':
-  app.run()
+HIGHEST_RATING = 5 
+highest_ratings_dataframe = ratings_dataframe.loc[ratings_dataframe["rating"] == HIGHEST_RATING,:]
+highest_ratings_dataframe
+len(highest_ratings_dataframe)
